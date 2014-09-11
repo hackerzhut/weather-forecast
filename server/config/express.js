1445128f602d8af3d32117449a5cf5892e25aef0
sara <hackerzhut.inc@gmail.com>
2014-09-12 06:41:15 +1000

    Removed outdated zlib

--- server/config/express.js
+++ server/config/express.js
@@ -21,7 +21,7 @@
   app.set('views', config.root + '/server/views');
   app.engine('html', require('ejs').renderFile);
   app.set('view engine', 'html');
-  //app.use(compression());
+  app.use(compression());
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: false }));
   app.use(methodOverride());
